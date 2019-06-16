028eff500c2a9bd26ffda090bb742df4383cfe84
Giovix92 <ggualtierone@gmail.com>
2019-06-16 17:15:57 +0200

    tissot: do not enable HAL3 by default
    Actually, this fixes whatsapp rear camera lag, because hal1 packagelist won't work properly.

--- prop.mk
+++ prop.mk
@@ -1,5 +1,4 @@
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
 vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap \
-persist.camera.dual.camera=0 \
-persist.camera.HAL3.enabled=1
+persist.camera.dual.camera=0
