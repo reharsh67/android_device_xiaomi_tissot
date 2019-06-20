f309952540635ac9c53fcd32c9b4351cb9e49d15
Giovanni Gualtieri <ggualtierone@gmail.com>
2019-06-20 13:55:30 +0200

    tissot: fix whatsapp camera lag
    Huge thanks to @Dil3mm4 for helping me finding this.
    Normally, camera.hal1.packagelist won't work. It should be vendor.camera.hal1.packagelist.

--- prop.mk
+++ prop.mk
@@ -2,4 +2,5 @@
 PRODUCT_PROPERTY_OVERRIDES += \
 vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap \
 persist.camera.dual.camera=0 \
-persist.camera.HAL3.enabled=1
+persist.camera.HAL3.enabled=1 \
+vendor.camera.hal1.packagelist=com.whatsapp
