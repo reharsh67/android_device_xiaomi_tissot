779665d9ce633a98fe414de32a6c0c880cf686ed
sooti <sooti85@gmail.com>
2019-06-25 15:25:02 +0200

    tissot: Use blacklist for aux
    On tissot there are too many apps to whitelist,
    blacklisting gcam should fix all the issues on al the cams
    
    Change-Id: I228c1e34223db1c363cd888bcb5b7e3c773f6478

--- prop.mk
+++ prop.mk
@@ -1,6 +1,7 @@
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap \
+vendor.camera.aux.packageblacklist=com.google.android.GoogleCamera \
+vendor.camera.hal1.packagelist=com.whatsapp \
 persist.camera.dual.camera=0 \
 persist.camera.HAL3.enabled=1 \
 vendor.camera.hal1.packagelist=com.whatsapp
